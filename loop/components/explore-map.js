58cf4ffa03510adfe3bb679f94ea9d9af09d7038
PUNOLEE <punolee@outlook.com>
2019-02-26 15:55:50 -0600

    deleted unneccessary lines

--- loop/components/explore-map.js
+++ loop/components/explore-map.js
@@ -1,14 +1,5 @@
 import React from "react";
-import { Image, View, StyleSheet,Dimensions } from "react-native";
-import { Container, Header, Content, Button, Text,Item, Accordion,Left,Right,Body,Icon,Title,Thumbnail,Input} from "native-base";
-import * as data from "../assets/data.json";
-import commonStyle from "../assets/styles/styles";
-import theme from "../assets/styles/theme.style";
 import { MapView } from 'expo';
-import { SearchBar } from "react-native-elements";
-
-const devicesWidth=Dimensions.get('window').width;
-const devicesHeight=Dimensions.get('window').height;
 
 export default class LoopMap extends React.Component {
   static navigationOptions = {
