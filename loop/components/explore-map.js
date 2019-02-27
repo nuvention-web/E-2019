ab84c171a742e091883f1519e3e683a1fab1b2e0
suojy <zhuzhubaobeiok@126.com>
2019-02-26 21:21:40 -0600

    added marker

--- loop/components/explore-map.js
+++ loop/components/explore-map.js
@@ -1,10 +1,18 @@
 import React from "react";
+import commonStyle from "../assets/styles/styles";
 import { MapView } from 'expo';
-
+import * as data from "../assets/marker.json";
+import {Card, CardItem, Text, Icon, Right,Left,Body} from "native-base";
 export default class LoopMap extends React.Component {
   static navigationOptions = {
     header: null
   };
+  constructor(props) {
+    super(props);
+    this.state={
+      markers: data.markers,
+ }
+ }
 
   render() {
     return (
@@ -17,7 +25,27 @@ export default class LoopMap extends React.Component {
           longitudeDelta: 0.0421,
         }}
         showsUserLocation = {true}
-      />
+      >
+          {this.state.markers.map(marker => ( 
+        <MapView.Marker key= {marker.key} coordinate={marker.coordinate}>
+     
+        <MapView.Callout>
+        
+            <CardItem>
+              <Body>
+              <Text style={commonStyle.text}>Title: {marker.title}{"\n"}
+        Place: {marker.description}</Text>
+        </Body>
+              <Right>
+              <Icon style={[commonStyle.Icon,{fontSize:40,marginLeft:15}]} name="add" />
+              </Right>
+             </CardItem>
+           
+        
+       
+        </MapView.Callout>
+        </MapView.Marker>))}
+      </MapView>
     );
   }
 }
