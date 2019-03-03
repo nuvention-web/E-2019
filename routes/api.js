0c34ea83434397d2d6036ff64bad7b0753876afb
Prerak Rustagi <prerak1992@gmail.com>
2019-03-03 17:04:52 -0600

    error handling added to getNearByLoops function

--- routes/api.js
+++ routes/api.js
@@ -33,28 +33,34 @@ module.exports = function (app) {
     req.log.info('Inside getNearByLoops functions');
     var lat = req.query.lat;
     var long = req.query.long;
-    var radius = req.query.rad || 1;
-    var unit = req.query.unit || 'mi';
-    app.dataRedis.georadius('maps:nearby:places', long, lat, radius, unit, 'WITHCOORD', 'WITHDIST', function(err, data) {
-      if(err) {
-        next(err);
-      } else {
-        var entities = [];
-        for(var i in data) {
-          entities[i] = {};
-          entities[i].title =  data[i][0];
-          entities[i].distance =  data[i][1];
-          entities[i].location =  {
-            "long": data[i][2][0],
-            "lat": data[i][2][1]
-          };
+    if(lat && long) {
+      var radius = req.query.rad || 1;
+      var unit = req.query.unit || 'mi';
+      app.dataRedis.georadius('maps:nearby:places', long, lat, radius, unit, 'WITHCOORD', 'WITHDIST', function(err, data) {
+        if(err) {
+          next(err);
+        } else {
+          var entities = [];
+          for(var i in data) {
+            entities[i] = {};
+            entities[i].title =  data[i][0];
+            entities[i].distance =  data[i][1];
+            entities[i].location =  {
+              "long": data[i][2][0],
+              "lat": data[i][2][1]
+            };
+          }
+          var resObj = { entities: entities };
+          resObj.count = entities.count;
+          res.send(resObj);
         }
-        var resObj = { entities: entities };
-        resObj.count = entities.count;
-        res.send(resObj);
+      });
+    } else {
+      var err = {
+        statusCode: 400, message: 'Bad Request! Query params lat and long and not defined'
       }
-    });
-
+      next(err);
+    }
   }
 
 };
