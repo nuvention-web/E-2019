6d4e3d8990840d99acb7b56b96f79462cc053eba
Prerak Rustagi <prerak1992@gmail.com>
2019-04-08 17:08:29 -0500

    master cleaned

--- loop/services/actions/messageActions.js
+++
@@ -1,9 +0,0 @@
-import {ADD_MESSAGE} from './types';
-
-export default function addMessage (message){
-    return{
-        type: ADD_MESSAGE,
-        message
-    }
-    
-}
--- loop/services/reducers/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, lyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducers from './rootReducers';
-
-export default function configureStore() {
-    return createStore(
-        rootReducers,
-    );
-}
