e9c635f03dd43305c708276241dd27f6d501ab7c
PUNOLEE <punolee@outlook.com>
2019-04-18 20:58:31 -0500

    theme applied

--- loop/src/components/home.js
+++ loop/src/components/home.js
@@ -6,6 +6,8 @@ import {
   MuiThemeProvider,
   createMuiTheme
 } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
@@ -14,9 +16,12 @@ import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import ChatIcon from "@material-ui/icons/Chat";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -24,8 +29,10 @@ import BookMarkIcon from "@material-ui/icons/BookmarkBorder";
 import HomeIcon from "@material-ui/icons/Home";
 import MainHome from "./homec";
 import CareerHome from "./homex";
-import Connection from "./connection"
-import { Route, Link,Switch, BrowserRouter as Router } from "react-router-dom";
+import Connection from "./connection";
+import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import Badge from "@material-ui/core/Badge";
 
 const drawerWidth = 240;
 
@@ -47,7 +54,30 @@ const mytheme = createMuiTheme({
   overrides: {
     MuiToolbar: {
       regular: {
-        backgroundColor: "#4281A4"
+        backgroundColor: "#fefcfe"
+      }
+    },
+    MuiAppBar: {
+      root: {
+        boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .03)"
+      }
+    },
+    MuiDrawer: {
+      paper: {
+        backgroundColor: "#3a394f"
+      }
+    },
+    MuiListItemText: {
+      primary: {
+        color: "#fefcfe"
+      }
+    },
+    MuiListItemIcon: {
+      root: {
+        color: "#9596aa"
+      },
+      secondary: {
+        color: "#8092c9"
       }
     }
   }
@@ -55,21 +85,17 @@ const mytheme = createMuiTheme({
 
 const styles = theme => ({
   root: {
-    display: "flex"
+    display: "flex",
+    color: "#fff"
+  },
+  grow: {
+    flexGrow: 1
   },
   appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
   },
   menuButton: {
     marginLeft: 12,
@@ -86,33 +112,72 @@ const styles = theme => ({
     width: drawerWidth
   },
   drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
+    // display: "flex",
+    //alignItems: "center",
+    backgroundColor: "#313344",
+    padding: "18px 18px",
+    ...theme.mixins.toolbar
+    //justifyContent: "flex-end"
+  },
+  drawerHeaderHeight: {
+    ...theme.mixins.toolbar
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
+    padding: theme.spacing.unit * 3
   },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.black, 0.05),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.black, 0.1)
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    color: theme.palette.common.black,
+    width: theme.spacing.unit * 9,
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "100%"
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    },
+    color: theme.palette.common.black
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
   }
 });
 
 class Home extends React.Component {
   state = {
-    open: false,
+    open: true,
     switch: false
   };
 
@@ -123,7 +188,6 @@ class Home extends React.Component {
   handleDrawerClose = () => {
     this.setState({ open: false });
   };
-  
 
   render() {
     const { classes, theme } = this.props;
@@ -132,102 +196,120 @@ class Home extends React.Component {
     return (
       <div className={classes.root}>
         <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open
-          })}
-        >
-          <MuiThemeProvider theme={mytheme}>
+        <MuiThemeProvider theme={mytheme}>
+          <AppBar position="fixed" className={classNames(classes.appBar)}>
             <Toolbar disableGutters={!open}>
-              <IconButton
-                color="inherit"
-                aria-label="Open drawer"
-                onClick={this.handleDrawerOpen}
-                className={classNames(classes.menuButton, open && classes.hide)}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" color="inherit" noWrap>
-                Loop
-              </Typography>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="Search…"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput
+                  }}
+                />
+              </div>
+              <div className={classes.grow} />
+              <div className={classes.sectionDesktop}>
+                <IconButton>
+                  <Badge badgeContent={17} color="secondary">
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton
+                  aria-owns="material-appbar"
+                  aria-haspopup="true"
+                  color="inherit"
+                >
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                    className={classes.avatar}
+                  />
+                </IconButton>
+              </div>
             </Toolbar>
+          </AppBar>
+        </MuiThemeProvider>
+        <nav className={classes.drawer}>
+          <MuiThemeProvider theme={mytheme}>
+            <Drawer
+              variant="persistent"
+              anchor="left"
+              open
+              classes={{
+                paper: classes.drawerPaper
+              }}
+            >
+              <div className={classes.drawerHeader}>
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  noWrap
+                  className={classes.brandname}
+                >
+                  LOOP
+                </Typography>
+              </div>
+              <List>
+                <ListItem button key={"Home"}>
+                  <ListItemIcon>
+                    <HomeIcon color="#9596aa" />
+                  </ListItemIcon>
+                  <ListItemText primary={"Home"} />
+                </ListItem>
+              </List>
+              <Divider />
+              <List>
+                <ListItem
+                  button
+                  key={"Journey"}
+                  onClick={() => this.props.history.push("/home/main")}
+                >
+                  <ListItemIcon>
+                    <BarChartIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Journey"} />
+                </ListItem>
+              </List>
+              <Divider />
+              <List>
+                <ListItem button key={"Connections"}>
+                  <ListItemIcon>
+                    <PermIdentityIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Connections"} />
+                </ListItem>
+              </List>
+              <List>
+                <ListItem button key={"Chat Rooms"}>
+                  <ListItemIcon>
+                    <ChatIcon style={{ fontSize: 22 }} />
+                  </ListItemIcon>
+                  <ListItemText primary={"Chat Rooms"} />
+                </ListItem>
+              </List>
+              <List>
+                <ListItem button key={"Calendar"}>
+                  <ListItemIcon>
+                    <CalendarTodayIcon style={{ fontSize: 22 }} />
+                  </ListItemIcon>
+                  <ListItemText primary={"Calendar"} />
+                </ListItem>
+              </List>
+            </Drawer>
           </MuiThemeProvider>
-        </AppBar>
-        <Drawer
-          className={classes.drawer}
-          variant="persistent"
-          anchor="left"
-          open={open}
-          classes={{
-            paper: classes.drawerPaper
-          }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={this.handleDrawerClose}>
-              {theme.direction === "ltr" ? (
-                <ChevronLeftIcon />
-              ) : (
-                <ChevronRightIcon />
-              )}
-            </IconButton>
-          </div>
-          <List>
-            <ListItem
-              button
-              key={"Home"}
-              onClick={() => this.props.history.push("/home/main")}
-            >
-              <ListItemIcon>
-                <HomeIcon />
-              </ListItemIcon>
-              <ListItemText primary={"Home"} />
-            </ListItem>
-          </List>
-          <Divider />
-          <List>
-            <ListItem
-              button
-              key={"UX Designer"}
-              onClick={() => this.props.history.push("/home/career")}
-            >
-              <ListItemIcon>
-                <BookMarkIcon />
-              </ListItemIcon>
-              <ListItemText primary={"UX Designer"} />
-            </ListItem>
-          </List>
-          <Divider />
-          <List>
-            <ListItem button key={"Frontend Manager"}>
-              <ListItemIcon>
-                <BookMarkIcon />
-              </ListItemIcon>
-              <ListItemText primary={"Frontend Engineer"} />
-            </ListItem>
-          </List>
-          <Divider />
-          <List>
-            <ListItem button key={"Fullstack Engineer"}>
-              <ListItemIcon>
-                <BookMarkIcon />
-              </ListItemIcon>
-              <ListItemText primary={"Fullstack Engineer"} />
-            </ListItem>
-          </List>
-        </Drawer>
-        <main
-          className={classNames(classes.content, {
-            [classes.contentShift]: open
-          })}
-        >
-        <div className={classes.drawerHeader} />
-            <Switch>
-              <Route path="/home/main" component={MainHome} />
-              <Route path="/home/career/connection" component={Connection}/>
-              <Route path="/home/career" component={CareerHome} />
-            </Switch>
-{/*          
+        </nav>
+        <main className={classNames(classes.content)}>
+          <div className={classes.drawerHeaderHeight} />
+          <Switch>
+            <Route path="/home/main" component={MainHome} />
+            <Route path="/home/career/connection" component={Connection} />
+            <Route path="/home/career" component={CareerHome} />
+          </Switch>
+          {/*          
           {this.state.switch ? <CareerHome /> : <MainHome />} */}
         </main>
       </div>
--- loop/src/components/homec.js
+++ loop/src/components/homec.js
@@ -6,8 +6,6 @@ import {
   MuiThemeProvider,
   createMuiTheme
 } from "@material-ui/core/styles";
-import Divider from "@material-ui/core/Divider";
-import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 const mytheme = createMuiTheme({
@@ -24,37 +22,41 @@ const mytheme = createMuiTheme({
     error: {
       main: "#FE938C"
     }
+  },
+  overrides: {
+    MuiPaper: {
+      elevation2: {
+        boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .05)"
+      }
+    }
   }
 });
 
 const styles = theme => ({
-  section1: {
-    margin: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2}px`
-  },
   section2: {
     margin: theme.spacing.unit * 2
   },
-  chip: {
-    margin: theme.spacing.unit
-  },
-  lightchip: {
-    margin: theme.spacing.unit,
-    backgroundColor: mytheme.palette.primary.light,
-    color: "#000"
-  },
   paper: {
     padding: theme.spacing.unit,
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    whiteSpace: "nowrap"
+    display: "flex",
+    flexDirection: "column",
+    color: theme.palette.text.secondary
   },
   skillset: {
+    marginTop: theme.spacing.unit * 3,
     flex: 1
+  },
+  papercontent: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  papercaption: {
+    display: "flex",
+    flexDirection: "column"
   }
 });
 
 class MainHome extends Component {
-
   handleDelete = () => {
     alert("You clicked the delete icon."); // eslint-disable-line no-alert
   };
@@ -67,108 +69,48 @@ class MainHome extends Component {
     const { classes } = this.props;
     return (
       <div>
-        <div className={classes.section1}>
-          <Typography gutterBottom variant="h4">
-            Hello, Puno
-          </Typography>
-        </div>
-
-        <Divider variant="middle" />
         <div className={classes.section2}>
           <Typography gutterBottom variant="h5">
-            Goal
-          </Typography>
-          <div>
-            <MuiThemeProvider theme={mytheme}>
-              <Chip
-                className={classes.chip}
-                color="secondary"
-                label="UX Designer"
-                onDelete={this.handleDelete}
-              />
-              <Chip
-                className={classes.chip}
-                color="secondary"
-                label="Frontend Engineer"
-                onDelete={this.handleDelete}
-              />
-              <Chip
-                className={classes.chip}
-                color="secondary"
-                label="Fullstack Engineer"
-                onDelete={this.handleDelete}
-              />
-              <Chip
-                className={classes.lightchip}
-                color="primary"
-                label="UI Designer"
-                onClick={this.handleClick}
-              />
-              <Chip
-                className={classes.lightchip}
-                color="primary"
-                label="Product Manager"
-                onClick={this.handleClick}
-              />
-              <Chip
-                className={classes.lightchip}
-                color="primary"
-                label="Software Engineer"
-                onClick={this.handleClick}
-              />
-              <Chip
-                className={classes.lightchip}
-                color="primary"
-                label="Data Scientist"
-                onClick={this.handleClick}
-              />
-            </MuiThemeProvider>
-          </div>
-        </div>
-
-        <Divider variant="middle" />
-        <div className={classes.section2}>
-          <Typography gutterBottom variant="h5">
-            Skillsets
+            Journey
           </Typography>
           <div className={classes.skillset}>
-            <Grid container spacing={24}>
-              <Grid item xs={2}>
-                <Paper className={classes.paper}>
-                  <Typography component="p">HTML/CSS</Typography>
-                </Paper>
+            <MuiThemeProvider theme={mytheme}>
+              <Grid container spacing={24}>
+                <Grid item xs={4}>
+                  <Paper className={classes.paper}>
+                    <Typography component="p">Total Touchpoints</Typography>
+                    <div className={classes.papercontent}>
+                      <div className={classes.papercaption}>
+                        <Typography variant="h6">6000</Typography>
+                        <Typography variant="caption">43%</Typography>
+                      </div>
+                    </div>
+                  </Paper>
+                </Grid>
+                <Grid item xs={4}>
+                  <Paper className={classes.paper}>
+                    <Typography component="p">Avg. Response Rate</Typography>
+                    <div className={classes.papercontent}>
+                      <div className={classes.papercaption}>
+                        <Typography variant="h6">41.2%</Typography>
+                        <Typography variant="caption">43%</Typography>
+                      </div>
+                    </div>
+                  </Paper>
+                </Grid>
+                <Grid item xs={4}>
+                  <Paper className={classes.paper}>
+                    <Typography component="p">Total Connections</Typography>
+                    <div className={classes.papercontent}>
+                      <div className={classes.papercaption}>
+                        <Typography variant="h6">603</Typography>
+                        <Typography variant="caption">53%</Typography>
+                      </div>
+                    </div>
+                  </Paper>
+                </Grid>
               </Grid>
-              <Grid item xs={2}>
-                <Paper className={classes.paper}>
-                  <Typography component="p">JavaScript/jQuery</Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs={2}>
-                <Paper className={classes.paper}>
-                  <Typography component="p">Vue.js</Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs={2}>
-                <Paper className={classes.paper}>
-                  <Typography component="p">React.js</Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs={2}>
-                <Paper className={classes.paper}>
-                  <Typography component="p">Python</Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs={2}>
-                <Paper className={classes.paper}>
-                  <Typography component="p">Java</Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs={2}>
-                <Paper className={classes.paper}>
-                  <Typography component="p">C++</Typography>
-                </Paper>
-              </Grid>
-            </Grid>
+            </MuiThemeProvider>
           </div>
         </div>
       </div>
