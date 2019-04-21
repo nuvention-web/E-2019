5459b5ca40642683a814595f5d76894f65c88e63
Rockwell Hsiang <dingxiang2015@u.northwestern.edu>
2019-04-20 22:33:10 -0500

    backend_app_setup

---
+++ loop-backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Loop makes your professional network better')
+});
+
+app.get('/api/loops/users', (req, res) =>{
+    var user1 = {firstName: 'David Lee', school: 'Kellog', careerGoal: 'Project Manager'}
+    res.send([user1])
+});
+
+app.listen(3000, () =>{
+    console.log("Loop server is listening on port 3000...")
+})
