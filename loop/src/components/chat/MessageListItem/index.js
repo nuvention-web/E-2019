3aa0670256a943fcaa532474684cf823c53a6f32
PUNOLEE <punolee@outlook.com>
2019-04-29 23:02:51 -0500

    edit message file

--- loop/src/components/chat/MessageListItem/index.js
+++ loop/src/components/chat/MessageListItem/index.js
@@ -1,120 +1,131 @@
-import React, { Component } from 'react';
-import Compose from '../Compose';
-import Toolbar from '../Toolbar';
-import ToolbarButton from '../ToolbarButton';
-import Message from '../Message';
-import moment from 'moment';
-
-import '../css/MessageList.css';
-import '../css/Toolbar.css';
-const MY_USER_ID = 'apple';
+import React, { Component } from "react";
+import Compose from "../Compose";
+import Toolbar from "../Toolbar";
+import ToolbarButton from "../ToolbarButton";
+import Message from "../Message";
+import moment from "moment";
+import { myFirestore, myFirebase } from "../../../firebase";
+import "../css/MessageList.css";
+import "../css/Toolbar.css";
 
 export default class MessageListItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: []
+      isLoading: true
     };
+    this.touserdata = this.props.data;
+    this.mess = [];
   }
 
   componentDidMount() {
-    this.getMessages();
+    this.getMess();
   }
 
-  getMessages = () => {
-    this.setState(prevState => {
-      return {
-        ...prevState,
-        messages: [
-          {
-            id: 1,
-            author: 'orange',
-            message: 'Hi David.',
-            timestamp: new Date().getTime()
-          },
-          {
-            id: 2,
-            author: 'apple',
-            message: 'Hi, I was wondering if you are going to the PM event in Chicago tomorrow?',
-            timestamp: new Date().getTime()
-          },
-          
-        ]
-      };
-    });
-  }
-
-  renderMessages() {
-    let i = 0;
-    let messageCount = this.state.messages.length;
-    let messages = [];
-
-    while (i < messageCount) {
-      let previous = this.state.messages[i - 1];
-      let current = this.state.messages[i];
-      let next = this.state.messages[i + 1];
-      let isMine = current.author === MY_USER_ID;
-      let currentMoment = moment(current.timestamp);
-      let prevBySameAuthor = false;
-      let nextBySameAuthor = false;
-      let startsSequence = true;
-      let endsSequence = true;
-      let showTimestamp = true;
-
-      if (previous) {
-        let previousMoment = moment(previous.timestamp);
-        let previousDuration = moment.duration(currentMoment.diff(previousMoment));
-        prevBySameAuthor = previous.author === current.author;
-        
-        if (prevBySameAuthor && previousDuration.as('hours') < 1) {
-          startsSequence = false;
-        }
-
-        if (previousDuration.as('hours') < 1) {
-          showTimestamp = false;
-        }
+  getMess = async () => {
+    var user = myFirebase.auth().currentUser;
+    if (user) {
+      const result = await myFirestore
+        .collection("messages")
+        .doc(user.uid)
+        .collection(this.touserdata.id)
+        .get();
+      if (result.docs.length > 0) {
+        this.mess = [...result.docs];
+        this.setState({ isLoading: false });
       }
-
-      if (next) {
-        let nextMoment = moment(next.timestamp);
-        let nextDuration = moment.duration(nextMoment.diff(currentMoment));
-        nextBySameAuthor = next.author === current.author;
-
-        if (nextBySameAuthor && nextDuration.as('hours') < 1) {
-          endsSequence = false;
-        }
-      }
-
-      messages.push(
-        <Message
-          key={i}
-          isMine={isMine}
-          startsSequence={startsSequence}
-          endsSequence={endsSequence}
-          showTimestamp={showTimestamp}
-          data={current}
-        />
-      );
-
-      // Proceed to the next message.
-      i += 1;
+    } else {
+      console.log("failed");
     }
+  };
 
-    return messages;
-  }
+
+  renderMess = () => {
+    if (this.mess.length > 0) {
+      var user = myFirebase.auth().currentUser;
+      if (user) {
+        let mess = [];
+        let i = 0;
+        let messageCount = this.mess.length;
+        while (i < messageCount) {
+          let currentMess = {}
+          let previous = this.mess[i - 1];
+          let current = this.mess[i];
+          let next = this.mess[i + 1];
+          let isMine = current.data().fromId === user.uid;
+          let currentMoment = moment(+current.data().timestamp);
+          let prevBySameAuthor = false;
+          let nextBySameAuthor = false;
+          let startsSequence = true;
+          let endsSequence = true;
+          let showTimestamp = true;
+          if (previous) {
+            let previousMoment = moment(+previous.data().timestamp);
+            let previousDuration = moment.duration(
+              currentMoment.diff(previousMoment)
+            );
+            prevBySameAuthor = previous.data().fromId === current.data().fromId;
+
+            if (prevBySameAuthor && previousDuration.as("hours") < 1) {
+              startsSequence = false;
+            }
+
+            if (previousDuration.as("hours") < 1) {
+              showTimestamp = false;
+            }
+          }
+          if (next) {
+            let nextMoment = moment(+next.data().timestamp);
+            let nextDuration = moment.duration(nextMoment.diff(currentMoment));
+            nextBySameAuthor = next.data().fromId === current.data().fromId;
+
+            if (nextBySameAuthor && nextDuration.as("hours") < 1) {
+              endsSequence = false;
+            }
+          }
+
+          currentMess["id"] = i;
+          currentMess["message"] = current.data().message;
+          currentMess["timestamp"] = currentMoment;
+          currentMess["author"] = current.data().fromId;
+          mess.push(
+            <Message
+              key={i}
+              isMine={isMine}
+              startsSequence={startsSequence}
+              endsSequence={endsSequence}
+              showTimestamp={showTimestamp}
+              data={currentMess}
+            />
+          );
+          i += 1;
+        }
+        return mess;
+      } else {
+        console.log("failed");
+      }
+    }else{
+      return null;
+    }
+  };
 
   render() {
     const { photourl, name } = this.props.data;
-    return(
+    return (
       <div className="message-list">
         <div className="toolbar">
-        <div className="left-items"><img className="conversation-photo" src={photourl} alt="conversation" /></div>
-        <h1 className="toolbar-title">{ name}</h1>
+          <div className="left-items">
+            <img
+              className="conversation-photo"
+              src={photourl}
+              alt="conversation"
+            />
+          </div>
+          <h1 className="toolbar-title">{name}</h1>
+        </div>
 
-      </div>
-
-        <div className="message-list-container">{this.renderMessages()}</div>
-        <Compose/>
+        <div className="message-list-container">{this.renderMess()}</div>
+        <Compose />
       </div>
     );
   }
