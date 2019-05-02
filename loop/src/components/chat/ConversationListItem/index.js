b4c5b1e5e7f2aa3232af6aa07df84900a11b934c
PUNOLEE <punolee@outlook.com>
2019-05-02 18:27:37 -0500

    edited chips of chatroom

--- loop/src/components/chat/ConversationListItem/index.js
+++ loop/src/components/chat/ConversationListItem/index.js
@@ -41,7 +41,8 @@ const styles = theme => ({
   },
   chip: {
     fontSize: 12,
-    marginLeft: theme.spacing.unit * 0.5
+    marginLeft: theme.spacing.unit * 0.5,
+    marginTop: theme.spacing.unit * 0.5
   }
 });
 
