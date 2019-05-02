642f94ed2bec9f4201daa7be56b64dbbe1f5c109
prerakrustagi <prerak1992@gmail.com>
2019-05-01 20:51:36 -0500

    api.js refactored.

--- routes/api.js
+++ routes/api.js
@@ -4,39 +4,51 @@ const api = express();
 var tsdb = require('../utils').tsdb;
 
 //test the queried function
-api.get('/loops/users/heatmap', function(req, res){
-  options = {
-    "metrics": "prerak",
-    "start_rel": "1_Y",
-    "tags": {
-      "type": ["human"]
-    },
-    "group_by": "type"
- }
-  tsdb.queryTS(options, function(err, res2){
-    result = res2;//the result from the KairosDB and the data is of format of KairosDB
-    res.json(result)
-  })
-})
-
-
-//function transfering JSON data queried from KairosDB
-//to JSON format that front end can read
-// function transformer(data) {
-// heat_data = [];
-// return heat_data
-// }
-
-
-// by getting the data with parameters userid, journey, and relative_start_time
-// the client end can get the data of that is readable for the heatmap.
-// api.get('/api/loops/users/heatmap/:userid/:journey/:rel_start_time', function(req, res){
-// tsdb(options, function(err, res){
-// result = res;//the result from the KairosDB and the data is of format of KairosDB
-// res.json(transformer())
-// })
-// })
 
 module.exports = function (app) {
-return new express.Router();
+    return new express.Router()
+    .get('/loops/users/heatmap', getHeatMap);
+
+
+    function getHeatMap(req, res, next) {
+        console.log('1')
+      options = {
+        "metric": "prerak",
+        "start_rel": "1_Y",
+        "tags": {
+          "type": ["human"]
+        },
+        "group_by": "type"
+     }
+      tsdb.queryTS(options, function(err, res2){
+          console.log('2')
+          console.log(res2);
+          console.log(err);
+        result = res2;//the result from the KairosDB and the data is of format of KairosDB
+        res.send(result)
+      });
+        console.log('3')
+    }
+
+
+
+    //function transfering JSON data queried from KairosDB
+    //to JSON format that front end can read
+    // function transformer(data) {
+    // heat_data = [];
+    // return heat_data
+    // }
+
+
+    // by getting the data with parameters userid, journey, and relative_start_time
+    // the client end can get the data of that is readable for the heatmap.
+    // api.get('/api/loops/users/heatmap/:userid/:journey/:rel_start_time', function(req, res){
+    // tsdb(options, function(err, res){
+    // result = res;//the result from the KairosDB and the data is of format of KairosDB
+    // res.json(transformer())
+    // })
+    // })
+
 };
+
+
