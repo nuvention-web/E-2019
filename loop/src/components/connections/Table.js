508c662be149321d379346fac9d2e974e3ce6f44
PUNOLEE <punolee@outlook.com>
2019-05-14 12:57:45 -0500

    table changed

--- loop/src/components/connections/Table.js
+++ loop/src/components/connections/Table.js
@@ -1,38 +1,46 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import Paper from '@material-ui/core/Paper';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import Paper from "@material-ui/core/Paper";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
 const styles = theme => ({
   root: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
+    overflowX: "auto"
   },
   table: {
-    minWidth: 700,
+    minWidth: 700
   },
+  wraptext:{
+    whiteSpace: "pre-line",
+  }
 });
 
 let id = 0;
 function createData(type, notes, date) {
   id += 1;
-  return { id, type, notes, date};
+  return { id, type, notes, date };
 }
 
 const rows = [
-  createData('Chat', "How are you", "May 10"),
-  createData('Email', "Introduction","May 10"),
-  createData('In-person', "Greeting", "May 09"),
-  createData('Social-Media', "Networking", "May 10"),
+  createData(
+    "Email",
+    "Robert and I discussed how I should prepare for my internship this summer working at Cruise. Takeaways:\n-Take advantage of events for autonomous transportation in SF\n-Read up on relevant news",
+    "May 13, 2019"
+  ),
+  createData("Phone", "Discussed my upcoming interview for Cruise Automation", "April 25, 2019"),
+  createData("In-person", "Learned more about Robert’s background:\r\n-Has a family with 2 sons, lives in Palo Alto\n-Loves to travel - going to Ecuador in July (Add to reminder - talking points)", "April 11, 201"),
+  createData("Chat", "Sent quick note about my current progress", "April 6, 2019"),
+  createData("Email", "Reached out through a connection from Stanford MBA program - Robert discussed his current role as Product Director at Waymo and consulting at incubator", "March 15, 2019")
 ];
 
 function SimpleTable(props) {
@@ -40,7 +48,7 @@ function SimpleTable(props) {
 
   return (
     <Paper className={classes.root}>
-    {/* <Tooltip title="Filter list">
+      {/* <Tooltip title="Filter list">
     <IconButton aria-label="Filter list">
       <FilterListIcon />
     </IconButton>
@@ -48,9 +56,9 @@ function SimpleTable(props) {
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
-            <TableCell>Activity Log Type</TableCell>
-            <TableCell align="right">Notes</TableCell>
-            <TableCell align="right">Date</TableCell>
+            <TableCell padding="dense">Log Type</TableCell>
+            <TableCell align="left">Notes</TableCell>
+            <TableCell align="left" padding="dense">Date</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -59,19 +67,18 @@ function SimpleTable(props) {
               <TableCell component="th" scope="row">
                 {row.type}
               </TableCell>
-              <TableCell align="right">{row.notes}</TableCell>
-              <TableCell align="right">{row.date}</TableCell>
+              <TableCell align="left" className={classes.wraptext}>{row.notes}</TableCell>
+              <TableCell align="left" padding="dense">{row.date}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      </Paper>
-   
+    </Paper>
   );
 }
 
 SimpleTable.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(SimpleTable);
